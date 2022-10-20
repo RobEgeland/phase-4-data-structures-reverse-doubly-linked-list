1c04191fffd7a58858e9a9b01a4849290afb21a6
Robert Egeland <robegeland@yahoo.com>
2022-10-20 18:35:18 -0500

    pass tests

--- lib/linked_list.rb
+++ lib/linked_list.rb
@@ -10,5 +10,31 @@ class LinkedList
 
   def reverse!
     # your code here
+    current = @head
+    temp = nil
+    if self.head == nil
+      return nil
+    end
+
+    while current != nil
+      temp = current.prev_node
+      current.prev_node = current.next_node
+      current.next_node = temp
+      current = current.prev_node
+    end
+    
+    if temp != nil
+      @head = temp.prev_node
+      while temp.next_node 
+        temp = temp.next_node
+      end
+      @tail = temp
+    end
+
+
+
+
+
+
   end
 end
